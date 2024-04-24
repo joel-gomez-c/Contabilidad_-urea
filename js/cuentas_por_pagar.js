26afe72b3dd99be8a8b4130ebe21bfb17fcc2792
Joelon <joelon2412@gmail.com>
2024-04-23 18:08:19 -0600

    Se agregaron detalles de estructura, estilo y funcionalidad para todas las páginas, también se modifico cada sidebar con el detalle que puede ir a cada página. Se le agregá a la página de cálculos de impuestos, las tablas correspondientes

---
+++ js/cuentas_por_pagar.js
@@ -0,0 +1,40 @@
+let texto1 = document.getElementById("texto1");
+let nombre = "";
+let enlace = "";
+let user_name = document.getElementById("user_name");
+let logOut = document.getElementById("logOut");
+
+window.addEventListener("load", function(event){
+    event.preventDefault();
+    if(this.localStorage.getItem("nombre")!=null){
+        nombre = String(this.localStorage.getItem("nombre"));
+        enlace = String(this.localStorage.getItem("enlace"));
+
+        let fecha = new Date();
+        let hora = fecha.getHours();
+        console.log(hora);
+        if(hora>=0 && hora<12){
+            //console.log("Buenos días");
+            texto1.innerText = `Buenos días ${nombre}`;
+        }else if(hora>=12 && hora<20){
+            //console.log("Buenas tardes");
+            texto1.innerText = `Buenas tardes ${nombre}`;
+        }else{
+            //console.log("Buenas noches");
+            texto1.innerText = `Buenas noches ${nombre}`;
+        }
+
+        user_name.innerText = `${nombre}`;
+
+    }//if != null
+
+});//window load
+
+logOut.addEventListener("click", function(event) {
+    event.preventDefault();
+    localStorage.removeItem("nombre");
+    localStorage.removeItem("enlace");
+    // localStorage.removeItem("usuario");
+    // localStorage.removeItem("contraseña");
+    window.location.href = "../html/login.html";
+});
