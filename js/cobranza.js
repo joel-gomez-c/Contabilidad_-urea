a3b8b60fe6419075022d2fca54d1a77793dd1d7a
Joelon <joelon2412@gmail.com>
2024-04-25 12:57:09 -0600

    Se agregaron las tablas y graficas (según sea el caso) en cobranza, cuentas por pagar y estados financieros

--- js/cobranza.js
+++ js/cobranza.js
@@ -1,8 +1,74 @@
-let texto1 = document.getElementById("texto1");
+// let texto1 = document.getElementById("texto1");
 let nombre = "";
 let enlace = "";
 let user_name = document.getElementById("user_name");
 let logOut = document.getElementById("logOut");
+
+var ctxOne = document.getElementById('chartOne').getContext('2d');
+var chartOne = new Chart(ctxOne, {
+    type: 'pie',
+    data: {
+        labels: ['IVA Suma de IMPUESTOS PAGADOS', 'ISR Suma de INGRESOS COBRADOS', 'ISR Suma de IMPUESTOS PAGADOS'],
+        datasets: [{
+            label: 'Dataset 1',
+            data: [40, 25, 25, 10],
+            backgroundColor: [
+                'blue',
+                'yellow',
+                'green',
+                'orange'
+            ]
+        }]
+    },
+    options: {
+        // plugins: {
+        //     legend: {
+        //         position: 'down'
+        //     }
+        // },
+        plugins: {
+            legend: {
+                display: false
+            }
+        },
+        responsive: false
+        // Configuración para la gráfica en 3D
+        // plugins: {
+        //     chartJsPlugin3d: {
+        //         enabled: true,
+        //         alpha: 45,
+        //         beta: 0
+        //     }
+        // }
+    }
+});
+
+var ctxTwo = document.getElementById('chartTwo').getContext('2d');
+var chartTwo = new Chart(ctxTwo, {
+    type: 'bar',
+    data: {
+        labels: ['Ernesto Eduardo Vargas Blanco', 'Público en general', 'Miguel Ángel Jarillo Girón', 'Estebán Emilio Perdome'],
+        datasets: [{
+            label: 'Total',
+            backgroundColor: 'blue',
+            borderColor: 'blue',
+            borderWidth: 1,
+            data: [6960, 18232, 19952, 32480]
+        }]
+    },
+    options: {
+        scales: {
+            x: {
+                stacked: true
+            },
+            y: {
+                stacked: true
+            }
+        },
+        indexAxis: 'y',
+        responsive: false
+    }
+});
 
 window.addEventListener("load", function(event){
     event.preventDefault();
@@ -10,19 +76,19 @@ window.addEventListener("load", function(event){
         nombre = String(this.localStorage.getItem("nombre"));
         enlace = String(this.localStorage.getItem("enlace"));
 
-        let fecha = new Date();
-        let hora = fecha.getHours();
-        console.log(hora);
-        if(hora>=0 && hora<12){
-            //console.log("Buenos días");
-            texto1.innerText = `Buenos días ${nombre}`;
-        }else if(hora>=12 && hora<20){
-            //console.log("Buenas tardes");
-            texto1.innerText = `Buenas tardes ${nombre}`;
-        }else{
-            //console.log("Buenas noches");
-            texto1.innerText = `Buenas noches ${nombre}`;
-        }
+        // let fecha = new Date();
+        // let hora = fecha.getHours();
+        // console.log(hora);
+        // if(hora>=0 && hora<12){
+        //     //console.log("Buenos días");
+        //     texto1.innerText = `Buenos días ${nombre}`;
+        // }else if(hora>=12 && hora<20){
+        //     //console.log("Buenas tardes");
+        //     texto1.innerText = `Buenas tardes ${nombre}`;
+        // }else{
+        //     //console.log("Buenas noches");
+        //     texto1.innerText = `Buenas noches ${nombre}`;
+        // }
 
         user_name.innerText = `${nombre}`;
 
--- js/estados_financieros.js
+++ js/estados_financieros.js
@@ -1,4 +1,4 @@
-let texto1 = document.getElementById("texto1");
+//let texto1 = document.getElementById("texto1");
 let nombre = "";
 let enlace = "";
 let user_name = document.getElementById("user_name");
@@ -10,19 +10,19 @@ window.addEventListener("load", function(event){
         nombre = String(this.localStorage.getItem("nombre"));
         enlace = String(this.localStorage.getItem("enlace"));
 
-        let fecha = new Date();
-        let hora = fecha.getHours();
-        console.log(hora);
-        if(hora>=0 && hora<12){
-            //console.log("Buenos días");
-            texto1.innerText = `Buenos días ${nombre}`;
-        }else if(hora>=12 && hora<20){
-            //console.log("Buenas tardes");
-            texto1.innerText = `Buenas tardes ${nombre}`;
-        }else{
-            //console.log("Buenas noches");
-            texto1.innerText = `Buenas noches ${nombre}`;
-        }
+        // let fecha = new Date();
+        // let hora = fecha.getHours();
+        // console.log(hora);
+        // if(hora>=0 && hora<12){
+        //     //console.log("Buenos días");
+        //     texto1.innerText = `Buenos días ${nombre}`;
+        // }else if(hora>=12 && hora<20){
+        //     //console.log("Buenas tardes");
+        //     texto1.innerText = `Buenas tardes ${nombre}`;
+        // }else{
+        //     //console.log("Buenas noches");
+        //     texto1.innerText = `Buenas noches ${nombre}`;
+        // }
 
         user_name.innerText = `${nombre}`;
 
