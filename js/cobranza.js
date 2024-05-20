37b74eee0c24f9aa680e8f612544a341bac58f10
Joelon <joelon2412@gmail.com>
2024-05-19 22:14:57 -0600

    Modificaciones del 19/MAY/2024

--- js/cobranza.js
+++ js/cobranza.js
@@ -70,31 +70,15 @@ var chartTwo = new Chart(ctxTwo, {
     }
 });
 
-window.addEventListener("load", function(event){
-    event.preventDefault();
+function iniciarPagina(){
     if(this.localStorage.getItem("nombre")!=null){
         nombre = String(this.localStorage.getItem("nombre"));
         enlace = String(this.localStorage.getItem("enlace"));
 
-        // let fecha = new Date();
-        // let hora = fecha.getHours();
-        // console.log(hora);
-        // if(hora>=0 && hora<12){
-        //     //console.log("Buenos días");
-        //     texto1.innerText = `Buenos días ${nombre}`;
-        // }else if(hora>=12 && hora<20){
-        //     //console.log("Buenas tardes");
-        //     texto1.innerText = `Buenas tardes ${nombre}`;
-        // }else{
-        //     //console.log("Buenas noches");
-        //     texto1.innerText = `Buenas noches ${nombre}`;
-        // }
-
         user_name.innerText = `${nombre}`;
 
     }//if != null
-
-});//window load
+}
 
 logOut.addEventListener("click", function(event) {
     event.preventDefault();
@@ -104,3 +88,61 @@ logOut.addEventListener("click", function(event) {
     // localStorage.removeItem("contraseña");
     window.location.href = "../html/login.html";
 });
+
+// import turnos from "./turnos.js";
+
+// const turnosContainer = document.getElementById("turnosContainer");
+// const detalleContainer = document.getElementById("detalleContainer");
+// let indiceSeleccionado;
+
+// const clienteElement = document.getElementById("cliente");
+// const modeloElement = document.getElementById("modelo");
+// const problemaElement = document.getElementById("problema");
+// const comentarioElement = document.getElementById("comentario");
+// const marcarTerminadoElement = document.getElementById("finalizar");
+
+
+// function createTarjeta(turno,index){
+//   const nuevaTarjeta = document.createElement("div");
+//   nuevaTarjeta.classList = "tarjeta";
+//   nuevaTarjeta.innerHTML = `
+//     <h3>${turno.cliente}</h3>
+//     <p>${turno.email}</p>
+//     <p>${turno.modelo}</p>
+//     <p>${turno.problema}</p>
+//   `
+//   nuevaTarjeta.addEventListener("click", ()=> actualizarDetalle(index))
+//   turnosContainer.appendChild(nuevaTarjeta);
+// }
+
+// function actualizarTarjetas(){
+//   turnosContainer.innerHTML = "";
+//   turnos.forEach((turno,i) => {
+//     createTarjeta(turno,i);
+//   })
+// }
+
+// function actualizarDetalle(index){
+//   if(indiceSeleccionado !== undefined) turnosContainer.children[indiceSeleccionado].classList.toggle("seleccionado",false);
+//   clienteElement.innerText = turnos[index].cliente;
+//   modeloElement.innerText = turnos[index].modelo;
+//   problemaElement.innerText = turnos[index].problema;
+//   detalleContainer.classList.toggle("escondido",false);
+//   indiceSeleccionado = index;
+//   turnosContainer.children[indiceSeleccionado].classList.toggle("seleccionado",true);
+// }
+
+// finalizar.addEventListener("click",()=> marcarTerminado(indiceSeleccionado))
+
+// async function marcarTerminado(i){
+//   const updateTurno = turnos[i];
+//   updateTurno.comentario = comentarioElement.value;
+//   const res = await editTurno(updateTurno.id,updateTurno);
+//   if(res.status === 200){
+//     turnos = turnos.filter(turno => turno.id !== updateTurno.id);
+//     indiceSeleccionado = 0;
+//     await actualizarTarjetas()
+//     detalleContainer.classList.toggle("escondido",true);
+//     comentarioElement.value="";
+//   }
+// }
---
+++ js/credenciales.js
@@ -0,0 +1,4 @@
+// TODO(developer): Set to client ID and API key from the Developer Console
+const CLIENT_ID = '1073732675694-ps4p447etalsvftoqf3i8tf7t81hj8pe.apps.googleusercontent.com';
+const API_KEY = 'AIzaSyA5rCJ3ErEiztGSR-X9zQDVuWSSiDDNdcs';
+const SPREADSHEET = "1xmcGjunkyttyxtvA8GE3tamlO-oEzFpVzyOZvp0bghQ";
