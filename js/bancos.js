d3596d2f6228af2ea46cd9411a67ae47ff2474f1
Joelon <joelon2412@gmail.com>
2024-05-24 12:09:36 -0600

    Cambios al 24 de mayo de 2024

--- js/bancos.js
+++ js/bancos.js
@@ -3,8 +3,39 @@ let nombre = "";
 let enlace = "";
 let user_name = document.getElementById("user_name");
 let logOut = document.getElementById("logOut");
-
 var ctx = document.getElementById('chartOne').getContext('2d');
+
+// TODO(developer): Set to client ID and API key from the Developer Console
+const CLIENT_ID = '932482928952-9mhu5qq6st6ta27rv2uo42df1kd5vati.apps.googleusercontent.com';
+const API_KEY = 'AIzaSyDkIub3BjxGpSGg6naz2fKB7uVowkW2INA';
+
+// Discovery doc URL for APIs used by the quickstart
+const DISCOVERY_DOC = 'https://sheets.googleapis.com/$discovery/rest?version=v4';
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+const SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly';
+
+let tokenClient;
+let gapiInited = false;
+let gisInited = false;
+
+document.getElementById("gapi").addEventListener("load", gapiLoaded);
+document.getElementById("gis").addEventListener("load", gisLoaded);
+
+// Get the table body element
+const tableBody = document.getElementById('tableOne').getElementsByTagName('tbody')[0];
+
+// Clear any existing rows
+tableBody.innerHTML = '';
+
+const tableBodyTwo = document.getElementById('tableTwo').getElementsByTagName('tbody')[0];
+tableBodyTwo.innerHTML = '';
+const tableBodyThree = document.getElementById('tableThree').getElementsByTagName('tbody')[0];
+tableBodyThree.innerHTML = '';
+const tableBodyFour = document.getElementById('tableFour').getElementsByTagName('tbody')[0];
+tableBodyFour.innerHTML = '';
+
 var chartOne = new Chart(ctx, {
     type: 'bar',
     data: {
@@ -43,20 +74,6 @@ window.addEventListener("load", function(event){
         nombre = String(this.localStorage.getItem("nombre"));
         enlace = String(this.localStorage.getItem("enlace"));
 
-        // let fecha = new Date();
-        // let hora = fecha.getHours();
-        // console.log(hora);
-        // if(hora>=0 && hora<12){
-        //     //console.log("Buenos días");
-        //     texto1.innerText = `Buenos días ${nombre}`;
-        // }else if(hora>=12 && hora<20){
-        //     //console.log("Buenas tardes");
-        //     texto1.innerText = `Buenas tardes ${nombre}`;
-        // }else{
-        //     //console.log("Buenas noches");
-        //     texto1.innerText = `Buenas noches ${nombre}`;
-        // }
-
         user_name.innerText = `${nombre}`;
 
     }//if != null
@@ -71,3 +88,249 @@ logOut.addEventListener("click", function(event) {
     // localStorage.removeItem("contraseña");
     window.location.href = "../html/login.html";
 });
+
+/**
+ * Callback after api.js is loaded.
+ */
+function gapiLoaded() {
+    gapi.load('client', initializeGapiClient);
+}
+
+/**
+ * Callback after the API client is loaded. Loads the
+ * discovery doc to initialize the API.
+ */
+async function initializeGapiClient() {
+    await gapi.client.init({
+        apiKey: API_KEY,
+        discoveryDocs: [DISCOVERY_DOC],
+    });
+    gapiInited = true;
+    maybeEnableButtons();
+}
+
+/**
+ * Callback after Google Identity Services are loaded.
+ */
+function gisLoaded() {
+    tokenClient = google.accounts.oauth2.initTokenClient({
+        client_id: CLIENT_ID,
+        scope: SCOPES,
+        callback: '', // defined later
+    });
+    gisInited = true;
+    maybeEnableButtons();
+}
+
+/**
+ * Enables user interaction after all libraries are loaded.
+ */
+function maybeEnableButtons() {
+    if (gapiInited && gisInited) {
+        //document.getElementById('authorize_button').style.visibility = 'visible';
+        handleAuthClick();
+    }
+}
+
+/**
+ *  Sign in the user upon button click.
+ */
+function handleAuthClick() {
+    tokenClient.callback = async (resp) => {
+        if (resp.error !== undefined) {
+            throw (resp);
+        }
+        //   document.getElementById('signout_button').style.visibility = 'visible';
+        //   document.getElementById('authorize_button').innerText = 'Refresh';
+        await listMajors();
+        await listMajorsTwo();
+        await listMajorsThree();
+        await listMajorsFour();
+    };
+
+    if (gapi.client.getToken() === null) {
+        // Prompt the user to select a Google Account and ask for consent to share their data
+        // when establishing a new session.
+        tokenClient.requestAccessToken({ prompt: 'consent' });
+    } else {
+        // Skip display of account chooser and consent dialog for an existing session.
+        tokenClient.requestAccessToken({ prompt: '' });
+    }
+}
+
+/**
+ *  Sign out the user upon button click.
+ */
+//   function handleSignoutClick() {
+//     const token = gapi.client.getToken();
+//     if (token !== null) {
+//       google.accounts.oauth2.revoke(token.access_token);
+//       gapi.client.setToken('');
+//       document.getElementById('content').innerText = '';
+//       document.getElementById('authorize_button').innerText = 'Authorize';
+//       document.getElementById('signout_button').style.visibility = 'hidden';
+//     }
+//   }
+
+/**
+ * Print the names and majors of students in a sample spreadsheet:
+ * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ */
+async function listMajors() {
+    let response;
+    try {
+        // Fetch first 10 files
+        response = await gapi.client.sheets.spreadsheets.values.get({
+            spreadsheetId: '1T5Djii_cGg0cyNHwYDJhg0S505Ok8h7jCDRBlZKoj1Y',
+            range: 'Bancos!B7:F11',
+        });
+    } catch (err) {
+        document.getElementById('content').innerText = err.message;
+        return;
+    }
+    const range = response.result;
+    if (!range || !range.values || range.values.length == 0) {
+        document.getElementById('content').innerText = 'No values found.';
+        return;
+    }
+    console.log(range.values);
+    // console.log(`Name: ${range.values[10][0]}, Major: ${range.values[10][4]}`);
+    // Flatten to string to display
+    // const output = range.values.reduce(
+    //     (str, row) => `${str}${row[0]}, ${row[4]}\n`,
+    //     'Name, Major:\n');
+    // document.getElementById('content').innerText = output;
+
+    // Iterate over each row in the range.values array
+    range.values.forEach(r => {
+        let row = `<tr>
+            <td>${r[0]}</td>
+            <td>${r[1]}</td>
+            <td>${r[2]}</td>
+            <td>${r[3]}</td>
+            <td>${r[4]}</td>
+            </tr>`;
+        tableBody.insertAdjacentHTML("beforeend", row);
+    });
+}
+
+async function listMajorsTwo() {
+    let response;
+    try {
+        // Fetch first 10 files
+        response = await gapi.client.sheets.spreadsheets.values.get({
+            spreadsheetId: '1T5Djii_cGg0cyNHwYDJhg0S505Ok8h7jCDRBlZKoj1Y',
+            range: 'Bancos!I3:S287',
+        });
+    } catch (err) {
+        document.getElementById('contentTwo').innerText = err.message;
+        return;
+    }
+    const range = response.result;
+    if (!range || !range.values || range.values.length == 0) {
+        document.getElementById('contentTwo').innerText = 'No values found.';
+        return;
+    }
+    console.log(range.values);
+    // console.log(`Name: ${range.values[10][0]}, Major: ${range.values[10][4]}`);
+    // Flatten to string to display
+    // const output = range.values.reduce(
+    //     (str, row) => `${str}${row[0]}, ${row[4]}\n`,
+    //     'Name, Major:\n');
+    // document.getElementById('content').innerText = output;
+
+    // Iterate over each row in the range.values array
+    range.values.forEach(r => {
+        let row = `<tr>
+            <td>${r[0]}</td>
+            <td>${r[1]}</td>
+            <td>${r[2]}</td>
+            <td>${r[3]}</td>
+            <td>${r[4]}</td>
+            <td>${r[5]}</td>
+            <td>${r[6]}</td>
+            <td>${r[7]}</td>
+            <td>${r[8]}</td>
+            <td>${r[9]}</td>
+            <td>${r[10]}</td>
+            </tr>`;
+        tableBodyTwo.insertAdjacentHTML("beforeend", row);
+    });
+}
+
+async function listMajorsThree() {
+    let response;
+    try {
+        // Fetch first 10 files
+        response = await gapi.client.sheets.spreadsheets.values.get({
+            spreadsheetId: '1T5Djii_cGg0cyNHwYDJhg0S505Ok8h7jCDRBlZKoj1Y',
+            range: 'Bancos!U3:AB14',
+        });
+    } catch (err) {
+        document.getElementById('contentThree').innerText = err.message;
+        return;
+    }
+    const range = response.result;
+    if (!range || !range.values || range.values.length == 0) {
+        document.getElementById('contentThree').innerText = 'No values found.';
+        return;
+    }
+    console.log(range.values);
+    // console.log(`Name: ${range.values[10][0]}, Major: ${range.values[10][4]}`);
+    // Flatten to string to display
+    // const output = range.values.reduce(
+    //     (str, row) => `${str}${row[0]}, ${row[4]}\n`,
+    //     'Name, Major:\n');
+    // document.getElementById('content').innerText = output;
+
+    // Iterate over each row in the range.values array
+    range.values.forEach(r => {
+        let row = `<tr>
+            <td>${r[0]}</td>
+            <td>${r[1]}</td>
+            <td>${r[2]}</td>
+            <td>${r[3]}</td>
+            <td>${r[4]}</td>
+            <td>${r[5]}</td>
+            <td>${r[6]}</td>
+            <td>${r[7]}</td>
+            </tr>`;
+        tableBodyThree.insertAdjacentHTML("beforeend", row);
+    });
+}
+
+async function listMajorsFour() {
+    let response;
+    try {
+        // Fetch first 10 files
+        response = await gapi.client.sheets.spreadsheets.values.get({
+            spreadsheetId: '1T5Djii_cGg0cyNHwYDJhg0S505Ok8h7jCDRBlZKoj1Y',
+            range: 'Bancos!AD2:AF14',
+        });
+    } catch (err) {
+        document.getElementById('contentFour').innerText = err.message;
+        return;
+    }
+    const range = response.result;
+    if (!range || !range.values || range.values.length == 0) {
+        document.getElementById('contentFour').innerText = 'No values found.';
+        return;
+    }
+    console.log(range.values);
+    // console.log(`Name: ${range.values[10][0]}, Major: ${range.values[10][4]}`);
+    // Flatten to string to display
+    // const output = range.values.reduce(
+    //     (str, row) => `${str}${row[0]}, ${row[4]}\n`,
+    //     'Name, Major:\n');
+    // document.getElementById('content').innerText = output;
+
+    // Iterate over each row in the range.values array
+    range.values.forEach(r => {
+        let row = `<tr>
+            <td>${r[0]}</td>
+            <td>${r[1]}</td>
+            <td>${r[2]}</td>
+            </tr>`;
+        tableBodyFour.insertAdjacentHTML("beforeend", row);
+    });
+}
