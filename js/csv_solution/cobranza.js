e0dcbac7cd2473ac1fcd630f81d37337b2186c41
Joelon <joelon2412@gmail.com>
2024-06-23 16:17:17 -0600

    Se hicieron pruebas con un documento csv para cobranza

--- js/csv_solution/cobranza.js
+++ js/csv_solution/cobranza.js
@@ -3,15 +3,19 @@ let nombre = "";
 let enlace = "";
 let user_name = document.getElementById("user_name");
 let logOut = document.getElementById("logOut");
+const url = '../../src/data/a_rosas/cobranza.csv';
 
-let arrayOne = [21272.16,16508.16,1600.00,1328.00,1120.00,1103.45,593.10,43524.87];
-let arrayTwo = [154223.16,107388.99,10408.33,8638.91,7285.83,8000.00,4300.00,300245.22];
-let arrayThree = ['ESTEBAN EMILIO PERDOME','DRIDCO MEXICO','ORIENTAL FILMS MEXICO','EN VIERNES PRODUCEN','MEDIA AND DESING LAB','ERNESTO EMILIO ORDOÑEZ GALLARDO','PUBLICO EN GENERAL','Total general'];
+let arrayOne = [21272.16, 16508.16, 1600.00, 1328.00, 1120.00, 1103.45, 593.10, 43524.87];
+let arrayTwo = [154223.16, 107388.99, 10408.33, 8638.91, 7285.83, 8000.00, 4300.00, 300245.22];
+let arrayThree = ['ESTEBAN EMILIO PERDOME', 'DRIDCO MEXICO', 'ORIENTAL FILMS MEXICO', 'EN VIERNES PRODUCEN', 'MEDIA AND DESING LAB', 'ERNESTO EMILIO ORDOÑEZ GALLARDO', 'PUBLICO EN GENERAL', 'Total general'];
 
 // Get the table body element
 const tableBody = document.getElementById('tableOne').getElementsByTagName('tbody')[0];
 // Clear any existing rows
 tableBody.innerHTML = '';
+
+var ctxOne = document.getElementById('chartOne').getContext('2d');
+var ctxTwo = document.getElementById('chartTwo').getContext('2d');
 
 window.addEventListener("load", function (event) {
     event.preventDefault();
@@ -52,87 +56,113 @@ function generateRandomColors(dataLength) {
     return colors;
 }
 
-range.values.forEach(r => {
-    const hasValues = r.some(cell => cell !== undefined && cell !== null && cell !== '');
-    if (hasValues) {
-        const rowData = r.map(cell => cell !== undefined ? cell : '');
-        console.log(rowData.length);
-        const row = `<tr>
-        <td>${rowData[0]}</td>
-        <td>${rowData[1]}</td>
-        <td>${rowData[2]}</td>
-        <td>${rowData[3]}</td>
-        <td>${rowData[4]}</td>
-        <td>${rowData[5]}</td>
+// Función para procesar cada fila
+// function processCsvRow(row) {
+//     return row.map(value => {
+//         // Quitar comillas dobles y espacios antes y después del valor
+//         value = value.trim().replace(/^"|"$/g, '').trim();
+//         // Reemplazar coma por nada y convertir a flotante si es un número
+//         if (/^\d+,\d{3}\.\d{2}$/.test(value)) {
+//             value = parseFloat(value.replace(/,/g, ''));
+//         }
+//         return value;
+//     });
+// }
+
+fetch(url)
+    .then(response => response.text())
+    .then(data => {
+        // Dividir el archivo CSV en filas
+        const rows = data.split('\n');
+        arrayOne = [];
+        arrayTwo = [];
+        arrayThree = [];
+        // Procesar cada fila (excepto la primera, que contiene los nombres de las columnas)
+        console.log(rows)
+        for (let i = 1; i < 6; i++) {
+            const row = rows[i].split(',');
+            //rows.length
+            console.log(row);
+            const fila = `<tr>
+        <td>${row[1]}</td>
+        <td>${row[2]}</td>
+        <td>${row[3]}</td>
+        <td>${row[4]}</td>
+        <td>${row[5]}</td>
+        <td>${row[6]}</td>
         </tr>`;
-        tableBody.insertAdjacentHTML("beforeend", row);
-        arrayOne.push(parseFloat(rowData[4].replace(/,/g, '')));
-        arrayTwo.push(parseFloat(rowData[5].replace(/,/g, '')));
-        arrayThree.push(rowData[0]);
-        //console.log(rowData);
-    }
-    //console.log(rowData);
-});
+            tableBody.insertAdjacentHTML("beforeend", fila);
+            arrayOne.push(parseFloat(row[5]));
+            arrayTwo.push(parseFloat(row[6]));
+            arrayThree.push(row[1]);
+        }
+        
+        console.log(arrayOne);
+        console.log(arrayTwo);
+        console.log(arrayThree);
 
-let backgroundColors = generateRandomColors(arrayOne.length);
-
-var ctxOne = document.getElementById('chartOne').getContext('2d');
-var chartOne = new Chart(ctxOne, {
-    type: 'pie',
-    data: {
-        labels: arrayThree.slice(0, arrayThree.length - 1),
-        datasets: [{
-            label: 'Dataset 1',
-            data: arrayOne.slice(0, arrayOne.length - 1),
-            backgroundColor: backgroundColors
-        }]
-    },
-    options: {
-        // plugins: {
-        //     legend: {
-        //         position: 'down'
-        //     }
-        // },
-        plugins: {
-            legend: {
-                display: false
-            }
-        },
-        responsive: true
-        // Configuración para la gráfica en 3D
-        // plugins: {
-        //     chartJsPlugin3d: {
-        //         enabled: true,
-        //         alpha: 45,
-        //         beta: 0
-        //     }
-        // }
-    }
-});
-
-var ctxTwo = document.getElementById('chartTwo').getContext('2d');
-var chartTwo = new Chart(ctxTwo, {
-    type: 'bar',
-    data: {
-        labels: arrayThree.slice(0, arrayThree.length - 1),
-        datasets: [{
-            label: 'Total',
-            backgroundColor: 'blue',
-            borderColor: 'blue',
-            borderWidth: 1,
-            data: arrayTwo.slice(0, arrayTwo.length - 1)
-        }]
-    },
-    options: {
-        scales: {
-            x: {
-                stacked: true
+        let backgroundColors = generateRandomColors(arrayOne.length);
+        
+        var chartOne = new Chart(ctxOne, {
+            type: 'pie',
+            data: {
+                labels: arrayThree.slice(0, arrayThree.length - 1),
+                datasets: [{
+                    label: 'Dataset 1',
+                    data: arrayOne.slice(0, arrayOne.length - 1),
+                    backgroundColor: backgroundColors
+                }]
             },
-            y: {
-                stacked: true
+            options: {
+                // plugins: {
+                //     legend: {
+                //         position: 'down'
+                //     }
+                // },
+                plugins: {
+                    legend: {
+                        display: false
+                    }
+                },
+                responsive: true
+                // Configuración para la gráfica en 3D
+                // plugins: {
+                //     chartJsPlugin3d: {
+                //         enabled: true,
+                //         alpha: 45,
+                //         beta: 0
+                //     }
+                // }
             }
-        },
-        indexAxis: 'y',
-        responsive: true
-    }
-});
+        });
+
+        var chartTwo = new Chart(ctxTwo, {
+            type: 'bar',
+            data: {
+                labels: arrayThree.slice(0, arrayThree.length - 1),
+                datasets: [{
+                    label: 'Total',
+                    backgroundColor: 'blue',
+                    borderColor: 'blue',
+                    borderWidth: 1,
+                    data: arrayTwo.slice(0, arrayTwo.length - 1)
+                }]
+            },
+            options: {
+                scales: {
+                    x: {
+                        stacked: true
+                    },
+                    y: {
+                        stacked: true
+                    }
+                },
+                indexAxis: 'y',
+                responsive: true
+            }
+        });
+
+    })
+    .catch(error => {
+        console.error('Error al cargar el archivo CSV:', error);
+    });
