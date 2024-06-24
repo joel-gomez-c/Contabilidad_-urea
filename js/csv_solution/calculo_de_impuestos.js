6bcf3471ad50ae3198ed15f62991d798dbc2afa9
Joelon <joelon2412@gmail.com>
2024-06-23 22:03:52 -0600

    Se modificaron la conexión con los datos en estados financieros y calculo de impuestos

--- js/csv_solution/calculo_de_impuestos.js
+++ js/csv_solution/calculo_de_impuestos.js
@@ -2,6 +2,7 @@ let nombre = "";
 let enlace = "";
 let user_name = document.getElementById("user_name");
 let logOut = document.getElementById("logOut");
+const url = '../../src/data/a_rosas/calculo_de_impuestos.csv';
 
 // Get the table body element
 const tableBody = document.getElementById('tableOne').getElementsByTagName('tbody')[0];
@@ -32,41 +33,58 @@ logOut.addEventListener("click", function(event) {
     window.location.href = "../../index.html";
 });
 
-// Iterate over each row in the range.values array
-range.values.forEach(r => {
-    let row = `<tr>
-    <td>${r[0]}</td>
-    <td>${r[1]}</td>
-    <td>${r[2]}</td>
-    <td>${r[3]}</td>
-    <td>${r[4]}</td>
-    <td>${r[5]}</td>
-    <td>${r[6]}</td>
-    <td>${r[7]}</td>
-    <td>${r[8]}</td>
-    <td>${r[9]}</td>
-    <td>${r[10]}</td>
-    <td>${r[11]}</td>
-    <td>${r[12]}</td>
-    </tr>`;
-    tableBody.insertAdjacentHTML("beforeend", row);
-});
+fetch(url)
+    .then(response => response.text())
+    .then(data => {
+        // Dividir el archivo CSV en filas
+        const rows = data.split('\n');
+        console.log(rows)
+        //Procesar cada fila (excepto la primera, que contiene los nombres de las columnas)
+        for (let i = 1; i < 17; i++) {
+            const row = rows[i].split(',');
+            //rows.length
+            console.log(row);
+            const fila = `<tr>
+        <td>${row[0]}</td>
+        <td>${row[1]}</td>
+        <td>${row[2]}</td>
+        <td>${row[3]}</td>
+        <td>${row[4]}</td>
+        <td>${row[5]}</td>
+        <td>${row[6]}</td>
+        <td>${row[7]}</td>
+        <td>${row[8]}</td>
+        <td>${row[9]}</td>
+        <td>${row[10]}</td>
+        <td>${row[11]}</td>
+        <td>${row[12]}</td>
+        </tr>`;
+            tableBody.insertAdjacentHTML("beforeend", fila);
+        }
 
-range.values.forEach(r => {
-    let row = `<tr>
-    <td>${r[0]}</td>
-    <td>${r[1]}</td>
-    <td>${r[2]}</td>
-    <td>${r[3]}</td>
-    <td>${r[4]}</td>
-    <td>${r[5]}</td>
-    <td>${r[6]}</td>
-    <td>${r[7]}</td>
-    <td>${r[8]}</td>
-    <td>${r[9]}</td>
-    <td>${r[10]}</td>
-    <td>${r[11]}</td>
-    <td>${r[12]}</td>
-    </tr>`;
-    tableBodyTwo.insertAdjacentHTML("beforeend", row);
-});
+        for (let i = 19; i < (rows.length-1); i++) {
+            const row = rows[i].split(',');
+            //rows.length
+            console.log(row);
+            const fila = `<tr>
+            <td>${row[0]}</td>
+            <td>${row[1]}</td>
+            <td>${row[2]}</td>
+            <td>${row[3]}</td>
+            <td>${row[4]}</td>
+            <td>${row[5]}</td>
+            <td>${row[6]}</td>
+            <td>${row[7]}</td>
+            <td>${row[8]}</td>
+            <td>${row[9]}</td>
+            <td>${row[10]}</td>
+            <td>${row[11]}</td>
+            <td>${row[12]}</td>
+            </tr>`;
+            tableBodyTwo.insertAdjacentHTML("beforeend", fila);
+        }
+        
+    })
+    .catch(error => {
+        console.error('Error al cargar el archivo CSV:', error);
+    });
--- js/csv_solution/estados_financieros.js
+++ js/csv_solution/estados_financieros.js
@@ -3,6 +3,7 @@ let nombre = "";
 let enlace = "";
 let user_name = document.getElementById("user_name");
 let logOut = document.getElementById("logOut");
+const url = '../../src/data/a_rosas/estados_financieros.csv';
 
 // Get the table body element
 const tableBody = document.getElementById('tableOne').getElementsByTagName('tbody')[0];
@@ -36,49 +37,73 @@ logOut.addEventListener("click", function(event) {
     window.location.href = "../../index.html";
 });
 
-range.values.forEach(r => {
-    let row = `<tr>
-    <td>${r[0]}</td>
-    <td>${r[1]}</td>
-    </tr>`;
-    tableBody.insertAdjacentHTML("beforeend", row);
-});
+fetch(url)
+    .then(response => response.text())
+    .then(data => {
+        // Dividir el archivo CSV en filas
+        const rows = data.split('\n');
+        console.log(rows)
+        //Procesar cada fila (excepto la primera, que contiene los nombres de las columnas)
+        for (let i = 1; i < 46; i++) {
+            const row = rows[i].split(',');
+            //rows.length
+            //console.log(row);
+            const fila = `<tr>
+        <td>${row[0]}</td>
+        <td>${row[1]}</td>
+        </tr>`;
+            tableBody.insertAdjacentHTML("beforeend", fila);
+        }
 
-range.values.forEach(r => {
-    let row = `<tr>
-    <td>${r[0]}</td>
-    <td>${r[1]}</td>
-    </tr>`;
-    tableBodyTwo.insertAdjacentHTML("beforeend", row);
-});
+        for (let i = 1; i < 45; i++) {
+            const row = rows[i].split(',');
+            //rows.length
+            //console.log(row);
+            const fila = `<tr>
+        <td>${row[4]}</td>
+        <td>${row[5]}</td>
+        </tr>`;
+            tableBodyTwo.insertAdjacentHTML("beforeend", fila);
+        }
 
-range.values.forEach(r => {
-    let row = `<tr>
-    <td>${r[0]}</td>
-    <td>${r[1]}</td>
-    <td>${r[2]}</td>
-    <td>${r[3]}</td>
-    <td>${r[4]}</td>
-    <td>${r[5]}</td>
-    <td>${r[6]}</td>
-    <td>${r[7]}</td>
-    <td>${r[8]}</td>
-    <td>${r[9]}</td>
-    <td>${r[10]}</td>
-    <td>${r[11]}</td>
-    <td>${r[12]}</td>
-    <td>${r[13]}</td>
-    <td>${r[14]}</td>
-    </tr>`;
-    tableBodyThree.insertAdjacentHTML("beforeend", row);
-});
+        for (let i = 1; i < 49; i++) {
+            const row = rows[i].split(',');
+            //rows.length
+            //console.log(row);
+            const fila = `<tr>
+            <td>${row[8]}</td>
+            <td>${row[9]}</td>
+            <td>${row[10]}</td>
+            <td>${row[11]}</td>
+            <td>${row[12]}</td>
+            <td>${row[13]}</td>
+            <td>${row[14]}</td>
+            <td>${row[15]}</td>
+            <td>${row[16]}</td>
+            <td>${row[17]}</td>
+            <td>${row[18]}</td>
+            <td>${row[19]}</td>
+            <td>${row[20]}</td>
+            <td>${row[21]}</td>
+            <td>${row[22]}</td>
+            </tr>`;
+            tableBodyThree.insertAdjacentHTML("beforeend", fila);
+        }
 
-range.values.forEach(r => {
-    let row = `<tr>
-    <td>${r[0]}</td>
-    <td>${r[1]}</td>
-    <td>${r[2]}</td>
-    <td>${r[3]}</td>
-    </tr>`;
-    tableBodyFour.insertAdjacentHTML("beforeend", row);
-});
+        for (let i = 1; i < 25; i++) {
+            const row = rows[i].split(',');
+            //rows.length
+            console.log(row);
+            const fila = `<tr>
+            <td>${row[25]}</td>
+            <td>${row[26]}</td>
+            <td>${row[27]}</td>
+            <td>${row[28]}</td>
+            </tr>`;
+            tableBodyFour.insertAdjacentHTML("beforeend", fila);
+        }
+
+    })
+    .catch(error => {
+        console.error('Error al cargar el archivo CSV:', error);
+    });
